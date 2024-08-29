b2c53e03046098686b07d45371744fb4727c50e0
ImranMahroof <mmimran084@gmail.com>
2024-08-29 17:18:45 +0530

    Updated dashboard

--- next-auth-test/app/mappg.tsx
+++ next-auth-test/app/mappg.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import React, { useState, useEffect, ChangeEvent } from "react";
@@ -10,6 +11,13 @@ import {
 import Papa from "papaparse";
 import { Upload, message, Table, Button } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
+import Sidebar from './dashboardpage/Sidebar'
+import { IconButton, Drawer } from "@material-tailwind/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Tables from'./dashboardpage/Table'
+import LoggedHeader from "./LoggedHeader";
+import Footer from "./Footer";
+
 
 const containerStyle = {
   width: "100%",
@@ -38,6 +46,18 @@ const MapComponent: React.FC = () => {
   const [tableData, setTableData] = useState<RouteData[]>([]);
   const [markers, setMarkers] = useState<{ lat: number; lng: number }[]>([]);
   const [optimizedRouteData, setOptimizedRouteData] = useState<{ key: string; label: string; coordinate: string }[]>([]);
+  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
+  const [open, setOpen] = useState(window.innerWidth < 960);
+  const openDrawer = () => setIsDrawerOpen(true);
+  const closeDrawer = () => setIsDrawerOpen(false);
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      setOpen(window.innerWidth < 960);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
 
   const handleFileUpload = (file: File) => {
@@ -177,18 +197,51 @@ const MapComponent: React.FC = () => {
     },
   ];
 
+  const tableData1 = [
+    { name: "Negombo", job: "Dehiwala", date: "Orugodawatta" },
+    { name: "Negombo", job: "Ragama", date: "Ja-Ela" },
+    { name: "Negombo", job: "Katunayake", date: "Kurana" },
+  ];
+
+  const tableData2 = [
+    { name: "Katunayake", job: "Kurana"},
+    { name: "Ragama", job: "Ja-Ela"},
+    { name: "Dehiwala", job: "Orugadawatta"},
+  ];
+
   return (
+    <>
+    <LoggedHeader />
+    <div style={{display: 'flex', marginTop:"70px"}}>
+    {open ? 
+      <div>
+        <IconButton variant="text" size="lg" onClick={openDrawer}>
+          {isDrawerOpen ? (
+            <XMarkIcon className="h-8 w-8 stroke-2" />
+          ) : (
+            <Bars3Icon className="h-8 w-8 stroke-2" />
+          )}
+        </IconButton>
+        <Drawer open={isDrawerOpen} onClose={closeDrawer}>
+          <Sidebar handleFileUpload={handleFileUpload} />
+        </Drawer>
+      </div>
+      : <Sidebar handleFileUpload={handleFileUpload}  />}
     <div style={{ width: '100%', maxWidth: '1200px' }}>
-      <Upload beforeUpload={handleFileUpload} showUploadList={false}>
+      {/* <Upload beforeUpload={handleFileUpload} showUploadList={false}>
         <Button type="primary" size="large" style={{marginBottom: '1rem'}} icon={<UploadOutlined />}>Upload CSV</Button>
-      </Upload>
+      </Upload> */}
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
         {directions && <DirectionsRenderer directions={directions} />}
         {markers.map((marker, index) => (
           <Marker key={index} position={marker} />
         ))}
       </GoogleMap>
-      <Table columns={columns} dataSource={tableData} rowKey="origin" style={{ marginTop: '20px' }} 
+      <h1>Uploaded Unoptimized Route</h1>
+      <Tables columns={["Origin", "Destination", "Waypoint"]} dataSource={tableData1} />
+      <h1>Optimized Route</h1>
+      <Tables columns={["Stop", "Coordinate"]} dataSource={tableData2} />
+      {/* <Table columns={columns} dataSource={tableData} rowKey="origin" style={{ marginTop: '20px' }} 
         title={() => 'Uploaded Unoptimized Route'}
       />
       <Table 
@@ -197,8 +250,12 @@ const MapComponent: React.FC = () => {
         pagination={false}
         style={{ marginTop: '20px' }} 
         title={() => 'Optimized Route'}
-      />
+      /> */}
+      
     </div>
+    </div>
+    <Footer/>
+    </>
   );
 };
 
